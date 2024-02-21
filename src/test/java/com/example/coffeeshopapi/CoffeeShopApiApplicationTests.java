4da045876f1b7ef7aaf4368501233bdda1b7bc7a
marianellagl <Marianella.gomezluna@gmail.com>
2024-02-21 18:13:16 -0300

    inicializacion

---
+++ src/test/java/com/example/coffeeshopapi/CoffeeShopApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.coffeeshopapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoffeeShopApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
